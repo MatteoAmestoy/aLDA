b598c36fc0b5a39856b4f6c61c18f0ed4817b40c
MatteoAmestoy <matteo.amestoy@gmail.com>
2020-03-23 17:00:30 +0100

    added topic uniqueness
    has to be checked on data

--- dev/model_comparison.py
+++ dev/model_comparison.py
@@ -6,7 +6,7 @@ Created on Mon Mar 23 11:22:35 2020
 """
 
 import numpy as np
-
+import time
 #%%
 
 class model_comparison():
@@ -23,8 +23,53 @@ class model_comparison():
         self.n_w = self.D[0]
         self.n_a,self.n_d =  self.A
         self.models = models
-    def train_models(self):
-        for m in self.models:
-            m.train()
-        return()            
 
+    def train_models(self):
+        '''
+        Train the models
+        '''
+        for m in self.models:
+            t0 = time.time()
+            print('--- Started training for '+m.name)
+            m.train(self.D, self.A, m.param)
+            print('finished after '+ str(time.time()-t0)+'seconds')
+            print('----------------------------------------')
+        return() 
+           
+
+    def compute_scores(self,scoreList):
+        
+        return()
+    
+    def perplexity(self,model):
+        
+        return()
+    
+    def coherence(self, model):
+        
+        return()
+    
+    def topic_uniqueness(self, model, L):
+        
+        top_words  = np.zeros((self.n_w,model.K))
+        for k in range(model.k):
+            top_words[np.argsort(model.phi)[-L:],k] = 1
+        count = np.sum(top_words,0)
+        return(np.sum(top_words/count)/model.K/L)
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

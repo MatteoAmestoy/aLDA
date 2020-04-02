7470bc2c0c9a217367356eb85004bcb37267b558
MatteoAmestoy <matteo.amestoy@gmail.com>
2020-04-02 10:08:25 +0200

    loading the wiki dataset using gensim preprocessing

--- data/wiki_read.py
+++ data/wiki_read.py
@@ -6,34 +6,37 @@ Created on Tue Mar 31 10:08:01 2020
 """
 
 import numpy as np
-import string
-from sklearn import preprocessing
-from nltk.stem.porter import *
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer
+from gensim.corpora import Dictionary
+import re
 #%%
 with open(r'C:\Users\Matteo\Documents\Git\aLDA\data\wikitext-2-raw\wiki.train.raw', encoding="utf8") as file:  
-    data = file.read()
-    
-    
+    dataRaw = file.read()
     
 #%%
-data = data.replace('= = = = ','+ + + +')
+data = dataRaw.replace('= = = = ','+ + + +')
 data = data.replace('= = =','- - -')
 data = data.replace('= =','* *')
 
 data = data.split(" = ")
+datagensim = []
+regex = re.compile('[^a-zA-Z ]')
+for d in data:
 
+    #First parameter is the replacement, second parameter is your input string
+    test = regex.sub('', d)
+    #Out: 'abdE'
+    if len(test)>100:
+        datagensim += [[i for i in test.split(" ") if len(i)>0]]
 #%%
-stopset = stopwords.words('english') 
-unwantedchar = string.punctuation + string.digits
 
-
-vectorizer = CountVectorizer( encoding='utf-8', decode_error='strict', strip_accents='ascii', lowercase=True, analyzer='word', max_df=0.8, min_df=4)
-X = vectorizer.fit_transform(data)
-
-#%%
-
-
-
+dct = Dictionary(datagensim)
+dct.filter_extremes(no_below=10, no_above=0.4 )
+dct.compactify()
+X = np.zeros((len(dct.keys()),len(datagensim)),int)
+i = 0
+for d in datagensim:
+    tmp = dct.doc2bow(d)
+    for key, value in tmp:
+        X[key,i] = value
+    i +=1
 
